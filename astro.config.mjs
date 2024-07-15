b72b8e8ad5da704fbcad5b39f98c09f1f69d6bc6
Dainwi <danwichoudhary@gmail.com>
2024-07-15 07:28:16 +0530

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,21 +6,10 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-import vercel from '@astrojs/vercel/serverless';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dainwi-blog.vercel.app/',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()],
-
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  }), sitemap()]
 });
-
-
-
-// export default defineConfig({
