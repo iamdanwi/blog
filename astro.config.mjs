0103b519c342214fbfeda44e4e04204f891a0d16
Dainwi <danwichoudhary@gmail.com>
2024-07-15 06:51:41 +0530

    vercel web analytics enabled

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,21 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dainwi-blog.vercel.app/',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }), sitemap()],
+
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
+
+
+
+// export default defineConfig({
